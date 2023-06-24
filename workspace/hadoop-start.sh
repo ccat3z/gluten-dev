c97370983c3fe10a4415aa99fa8ab78caef84abd
Lingfeng Zhang <lingfeng.zhang@intel.com>
2023-06-24 13:54:07 +0000

    Enable hadoop rbf

--- workspace/hadoop-start.sh
+++ workspace/hadoop-start.sh
@@ -19,7 +19,13 @@ for i in 1 3; do
     ssh hadoop$i $HADOOP_HOME/bin/hdfs --daemon stop namenode
 done
 
-sudo $HADOOP_HOME/bin/hdfs zkfc -formatZK
+for i in 1 3; do
+    ssh hadoop$i sudo $HADOOP_HOME/bin/hdfs zkfc -formatZK
+done
 
 sudo $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-yarn.sh
+
+for i in {1..6}; do
+    ssh hadoop$i $HADOOP_HOME/bin/hdfs --daemon start dfsrouter
+done
